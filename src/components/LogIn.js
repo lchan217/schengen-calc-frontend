8b0313d8de0c9d0e109b504224b1951824e8bf2d
Lizzie <lchan217@gmail.com>
2020-04-23 17:40:25 -0700

    add form, handlechange

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
 
-const LogIn = () => {
-  return <div>Login Home page</div>;
-};
+class LogIn extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+  render() {
+    return (
+      <div>
+        <Form className='w-50 p-5'>
+          <Form.Group>
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              onChange={this.handleChange}
+              type='email'
+              name='email'
+              placeholder='Email'
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              onChange={this.handleChange}
+              type='password'
+              name='password'
+              placeholder='Password'
+            />
+          </Form.Group>
+          <Button variant='primary' type='submit'>
+            Submit
+          </Button>
+          <br />
+          Click here to create new
+          <br />
+          Authentication
+        </Form>
+      </div>
+    );
+  }
+}
 
 export default LogIn;
