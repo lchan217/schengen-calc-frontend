50db6992464093ca080098ad08fd219723a7e761
Lizzie <lchan217@gmail.com>
2020-05-09 16:47:56 -0700

    add handlechange for newtrip form

--- src/components/NewTrip/NewTrip.js
+++ src/components/NewTrip/NewTrip.js
@@ -3,21 +3,50 @@ import { Form, Container, Button } from "react-bootstrap";
 import "./NewTrip.css";
 
 class NewTrip extends Component {
+  constructor() {
+    super();
+    this.state = {
+      destination: "",
+      entry: "",
+      exit: "",
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
   render() {
+    const { handleChange } = this;
     return (
       <Container className='new-trip-container'>
         <Form className='p-4'>
           <Form.Group controlId='destination'>
             <Form.Label>Destination</Form.Label>
-            <Form.Control type='text' placeholder='Enter destination' />
+            <Form.Control
+              onChange={handleChange}
+              type='text'
+              name='destination'
+              placeholder='Enter destination'
+            />
           </Form.Group>
           <Form.Group controlId='entry'>
             <Form.Label>Entry</Form.Label>
-            <Form.Control type='date' placeholder='Enter date of entry' />
+            <Form.Control
+              onChange={handleChange}
+              type='date'
+              name='entry'
+              placeholder='Enter date of entry'
+            />
           </Form.Group>
           <Form.Group controlId='exit'>
             <Form.Label>Exit</Form.Label>
-            <Form.Control type='date' placeholder='Enter date of exit' />
+            <Form.Control
+              onChange={handleChange}
+              type='date'
+              name='exit'
+              placeholder='Enter date of exit'
+            />
           </Form.Group>
           <Form.Text className='text-muted'>
             All fields must be filled out
